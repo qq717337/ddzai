c285824b306051c4c50fb64527db0a55a849b862
hifior <625673575@qq.com>
2018-03-11 19:59:14 +0800

    下载

--- DDZ_AI/SmoothCard.cpp
+++ DDZ_AI/SmoothCard.cpp
@@ -26,88 +26,88 @@ void SmoothCard::ExchangeSet(MinStepSplitStrategy* splitStrategy, HandCards* add
 	splitType.RequireFromAll(swapCount, _single, _double, _triple, requireFilter);
 	for (uint8_t cardIndex : _single) {
 		CardVector subValue = removeSet->GetCardsValue(cardIndex, 1);
-			for (auto v : subValue) {
-				if (cc < swapCount) {
-					++cc;
-					removeSet->RemoveCard(v);
-					addSet->AddCard(v);
-				}
+		for (auto v : subValue) {
+			if (cc < swapCount) {
+				++cc;
+				removeSet->RemoveCard(v);
+				addSet->AddCard(v);
 			}
 		}
-		for (uint8_t cardIndex : _double) {
-			CardVector subValue = removeSet->GetCardsValue(cardIndex, 2);
-				for (auto v : subValue) {
-					if (cc < swapCount) {
-						++cc;
-						removeSet->RemoveCard(v);
-						addSet->AddCard(v);
-					}
-				}
+	}
+	for (uint8_t cardIndex : _double) {
+		CardVector subValue = removeSet->GetCardsValue(cardIndex, 2);
+		for (auto v : subValue) {
+			if (cc < swapCount) {
+				++cc;
+				removeSet->RemoveCard(v);
+				addSet->AddCard(v);
 			}
-			for (uint8_t cardIndex : _triple) {
-				CardVector subValue = removeSet->GetCardsValue(cardIndex, 3);
-				for (auto v : subValue) {
-					if (cc < swapCount) {
-						++cc;
-						removeSet->RemoveCard(v);
-						addSet->AddCard(v);
-					}
-				}
-			}
-			_ASSERT(cc == swapCount);
 		}
-		void SmoothCard::Optimized(DecorateDealStrategy * strategy, std::vector <int>Epch)
-		{
-			ResetPlayerCard(0);
-			if (strategy != nullptr)
-				strategy->PreDeal();
+	}
+	for (uint8_t cardIndex : _triple) {
+		CardVector subValue = removeSet->GetCardsValue(cardIndex, 3);
+		for (auto v : subValue) {
+			if (cc < swapCount) {
+				++cc;
+				removeSet->RemoveCard(v);
+				addSet->AddCard(v);
+			}
+		}
+	}
+	_ASSERT(cc == swapCount);
+}
+void SmoothCard::Optimized(DecorateDealStrategy * strategy, std::vector <int>Epch)
+{
+	ResetPlayerCard(0);
+	if (strategy != nullptr)
+		strategy->PreDeal();
 
-			RandomFillLeft();
-			for (auto v : Epch) {
-				if (v > 0) {
-					Process(v);
-				}
-				else {
-					ProcessDesk();
-				}
+	RandomFillLeft();
+	for (auto v : Epch) {
+		if (v > 0) {
+			Process(v);
+		}
+		else {
+			ProcessDesk();
+		}
 #if _DEBUG
-			auto secondTime = ToString();
+		auto secondTime = ToString();
 #endif
-			}
-			if (PlayerCardSet[0]->Size() != PlayerCardSet[1]->Size() || PlayerCardSet[0]->Size() != PlayerCardSet[2]->Size()) {
-				throw std::runtime_error("返回的张数不一致");
-		}
-		}
-		void SmoothCard::Process(int swapCount)
-		{
-			for (int k = 0; k < 3; k++) {
-				int nextK = (k + 1) % 3;
-				Update();
-				MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[k]->Data()));
-				ExchangeSet(&splitStrategy, PlayerCardSet[nextK], PlayerCardSet[k], swapCount);
-			}
+	}
+	if (PlayerCardSet[0]->Size() != PlayerCardSet[1]->Size() || PlayerCardSet[0]->Size() != PlayerCardSet[2]->Size()) {
+		throw std::runtime_error("返回的张数不一致");
+	}
+}
+void SmoothCard::Process(int swapCount)
+{
+	for (int k = 0; k < 3; k++) {
+		int nextK = (k + 1) % 3;
+		Update();
+		MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[k]->Data()));
+		ExchangeSet(&splitStrategy, PlayerCardSet[nextK], PlayerCardSet[k], swapCount);
+	}
 
-			Update();
-		}
+	Update();
+}
 
-		void SmoothCard::ProcessDesk()
-		{
-			for (auto v : ExtraCard.Data()) {
-				PlayerCardSet[0]->AddCard(v);
-			}
-			for (int k = 0; k < 3; ++k) {
-				Update();
-				MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[k]->Data()));
-				ExchangeSet(&splitStrategy, PlayerCardSet[(k + 1) % 3], PlayerCardSet[k], 3);
-			}
-			ExtraCard.Reset(false);
-			Update();
-			MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[0]->Data()));
-			ExchangeSet(&splitStrategy, &ExtraCard, PlayerCardSet[0], 3);
-			Update();
-		}
+void SmoothCard::ProcessDesk()
+{
+	for (auto v : ExtraCard.Data()) {
+		PlayerCardSet[0]->AddCard(v);
+	}
+	for (int k = 0; k < 3; ++k) {
+		Update();
+		MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[k]->Data()));
+		ExchangeSet(&splitStrategy, PlayerCardSet[(k + 1) % 3], PlayerCardSet[k], 3);
+	}
+	ExtraCard.Reset(false);
+	Update();
+	MinStepSplitStrategy splitStrategy(std::make_shared<HandCards>(PlayerCardSet[0]->Data()));
+	ExchangeSet(&splitStrategy, &ExtraCard, PlayerCardSet[0], 3);
+	Update();
+}
 
 
-		SmoothCard::~SmoothCard()
-		{
-		}
+SmoothCard::~SmoothCard()
+{
+}
